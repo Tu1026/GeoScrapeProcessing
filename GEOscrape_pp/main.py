0a1411bfe202e092edd1658db09c88417ab54ddf
WilsonTu <s31302@gmail.com>
2021-09-13 10:41:39 -0700

    finsihed testing click, sucessfully added commands to cli

--- GEOscrape_pp/main.py
+++ GEOscrape_pp/main.py
@@ -1,16 +1,30 @@
 import click
+##Builiding command line options. Cannot figure out how to modularize click options
 
+version = 0.01
+softwareName = "Post GEO Filter"
+
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _add_options
+
+credentials_option = [click.option('-p','--password', help="GEMMA password"), 
+click.option('-u','--username', required=True, help="GEMMA username")]
 
 @click.group()
-@click.option('-p','--password', required=True, help="GEMMA password")
-@click.option('-u','--username', required=True, help="GEMMA username")
 def cli():
-    pass
+    print(f'{softwareName} =========================================== version: {version} initiating')
 
-@cli.command()
-def test():
-    print("worked")
+@cli.command("test")
+@add_options(credentials_option)
+def test(**kwargs):
+    print(kwargs)
 
-
+@cli.command("test2")
+def test2():
+    print("test2")
 if __name__ == "__main__":
     cli()
