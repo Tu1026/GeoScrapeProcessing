354ca417a2cc860a7254f9e07f2cfcbd5ba59e23
WilsonTu <s31302@gmail.com>
2021-09-13 15:10:53 -0700

    implementing the extracting useful text column function

--- GEOscrape_pp/src/Filters/RNATypeFilter.py
+++ GEOscrape_pp/src/Filters/RNATypeFilter.py
@@ -1,3 +1,9 @@
 from filter import Filter
+
 class RNAtypeFilter(Filter):
+
+    def __init__(self, df) -> None:
+        super().__init__(df)
     
+    ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
+    def lnt
--- GEOscrape_pp/src/Filters/filter.py
+++ GEOscrape_pp/src/Filters/filter.py
@@ -1,3 +1,16 @@
 class Filter:
-    def __init__(self) -> None:
+    df = None
+
+    ## Which column starts containing useful text, 12 for now
+    text_index_start = 12
+    ## Which column stops containing useful text, 15 for now
+    text_index_stop = 15
+    def __init__(self, df) -> None:
         pass
+
+    def extractTextColumn(self):
+        return self.df.iloc[:self.text_index_start, self.text_index_stop]
+
+    ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
+    def filterTerms(self, terms):
+        
