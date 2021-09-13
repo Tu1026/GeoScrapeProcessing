5330b230fcc03bdc206a3e5019bc591191b148fa
WilsonTu <s31302@gmail.com>
2021-09-13 11:49:00 -0700

    change functions into a reader class for reusability

--- GEOscrape_pp/src/reader.py
+++ GEOscrape_pp/src/reader.py
@@ -1,17 +1,22 @@
 # Command line should be in format: terms.txt tsvofscrape
 
-def open_geoscrape(geoscrape):
-    """
-    Open up a tab delimited GEOScrape for downstream filtering
-    """
-    # Pandas to open data frame of our listGEOData output
-    df = pd.read_csv(geoscrape, sep = "\t")
-    return(df)
+class Reader:
+    def __init__(self) -> None:
+        print("Initiating the reader")
+        import pandas as pd
 
-def open_terms(terms):
-    """
-    Open up a \n delimited list of terms which you consider hits
-    """
-    with open(terms, "r") as reader:
-        my_terms = reader.readlines()
-        return(my_terms)
+    def pandas_read(file):
+        """
+        Open up a tab delimited GEOScrape for downstream filtering
+        """
+        # Pandas to open data frame of our listGEOData output
+        df = pd.read_csv(file, sep = "\t")
+        return(df)
+
+    def open_terms(file):
+        """
+        Open up a \n delimited list of terms which you consider hits
+        """
+        with open(file, "r") as f:
+            my_terms = f.readlines()
+            return(my_terms)
