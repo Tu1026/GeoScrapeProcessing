cb6d08cddf4f3d9b6deb99e1761838cdf56aff23
WilsonTu <s31302@gmail.com>
2021-09-14 14:18:34 -0700

    added default args for filters

--- GEOscrape_pp/src/filters/RNATypeFilter.py
+++ GEOscrape_pp/src/filters/RNATypeFilter.py
@@ -1,7 +1,10 @@
 from filter import Filter
 
 class RNAtypeFilter(Filter):
-
+    ## The reason why something is filtered out at this stage
+    failed_reason = "Given Experiment does not use an RNA techonology supported"
+    ## The regex terms for unwanted hit terms
+    regex_terms = "scRNA|single.cell.RNA|locRNA|lon.non.coding.rna|mirna|microRNA"
     def __init__(self, df) -> None:
         super().__init__(df, "RNA")
     
