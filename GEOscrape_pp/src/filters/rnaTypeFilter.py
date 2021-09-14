de7b8f67671ac45fb272b1aa9111255ecd4f7d62
WilsonTu <s31302@gmail.com>
2021-09-14 16:16:03 -0700

    name change camelcase

--- GEOscrape_pp/src/filters/rnaTypeFilter.py
+++ GEOscrape_pp/src/filters/rnaTypeFilter.py
@@ -1,12 +1,19 @@
-from filter import Filter
+from re import I
+from .filter import Filter
 
-class RNAtypeFilter(Filter):
+class RNATypeFilter(Filter):
     ## The reason why something is filtered out at this stage
     failed_reason = "Given Experiment does not use an RNA techonology supported"
     ## The regex terms for unwanted hit terms
     regex_terms = "scRNA|single.cell.RNA|locRNA|lon.non.coding.rna|mirna|microRNA"
+    filterType = "RNA"
+    
     def __init__(self, df) -> None:
-        super().__init__(df, "RNA")
+        super().__init__(df, self.filterType)
     
     ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
-    def 
+    def filterTerms(self):
+        return super().filterTerms(self.regex_terms, self.failed_reason)
+    
+    def test(self):
+        print("test")
