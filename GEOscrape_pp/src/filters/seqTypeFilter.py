bab6996ce8a7c8cc2e02f0132591138928848f68
WilsonTu <s31302@gmail.com>
2021-09-16 13:39:31 -0700

    added seqtypefilter

---
+++ GEOscrape_pp/src/filters/seqTypeFilter.py
@@ -0,0 +1,20 @@
+
+from .filter import Filter
+
+class seqTypeFilter(Filter):
+    ## The reasouperSerien why something is filtered out at this stage
+    failedReason = "These experiments are array experiements that don't to be on RNA-seq pipeline"
+    successReason = "These are RNA-seq experiments that need to be in the pipeline"
+    ## The regex terms for unwanted hit terms
+    regex_terms = "array"
+    filterType = "Superseries"
+    relevantFields = ["Type"]
+    
+    def __init__(self, df) -> None:
+        super().__init__(df, self.filterType, self.relevantFields)
+    
+    ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api
+    def filterTerms(self):
+        super().filterTerms(self.regex_terms, self.failedReason, self.successReason)
+
+    
