5b65d8e990cabff4a0024e5805f53f7f83dd601c
WilsonTu <s31302@gmail.com>
2021-09-16 14:47:52 -0700

    gemmaService template

---
+++ GEOscrape_pp/src/misc/gEMMAService.py
@@ -0,0 +1,13 @@
+import requests
+import base64
+
+class GEMMAService:
+    usename = ""
+    password = ""
+
+    
+    def __init__(self, username, password):
+        self.usename = username
+        self.password = password
+
+
