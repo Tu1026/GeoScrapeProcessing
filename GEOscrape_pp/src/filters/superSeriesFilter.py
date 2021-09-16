c8586df53fc39ac8be68e167fa360db219147b10
WilsonTu <s31302@gmail.com>
2021-09-16 12:26:55 -0700

    Change to compatiable with parent class

--- GEOscrape_pp/src/filters/superSeriesFilter.py
+++ GEOscrape_pp/src/filters/superSeriesFilter.py
@@ -1,19 +1,20 @@
-from typing import overload
+## This is a hidden filter, output is not important because we simply just don't want superseries
+
 from .filter import Filter
 
 class SuperSeriesFilter(Filter):
     ## The reason why something is filtered out at this stage
     failed_reason = "Given Experiment is a superseries"
-    ## The regex terms for unwanted hit terms
-    regex_terms = "s"
+    ## The regex terms not applicable here 
+    regex_terms = "TRUE"
     filterType = "Superseries"
+    relevantFields = ["SuperSeries"]
     
     def __init__(self, df) -> None:
-        super().__init__(df, self.filterType)
+        super().__init__(df, self.filterType, self.relevantFields)
     
     ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api
     def filterTerms(self):
-        df = super().returnFrame()
-        super().df = df.loc[df["SuperSeries"] == False]
+        super().filterTerms(self.regex_terms, self.failed_reason)
 
 
