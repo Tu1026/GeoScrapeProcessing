7cde7a41fd93b9177ef6ed86bc6e602789dd7209
WilsonTu <s31302@gmail.com>
2021-09-16 14:41:27 -0700

    template classes

---
+++ GEOscrape_pp/src/filters/nonCuratePlatFilter.py
@@ -0,0 +1,19 @@
+from filters import Filter
+
+class nonCuratedPlatFilter(Filter):
+    ## The reason why something is filtered out at this stage
+    failedReason = "Given experiment uses platforms not already curated in GEMMA"
+    ## Why experiemnt passed filter
+    successReason = "Supported RNA type"
+    ## The regex terms for unwanted hit terms
+    regex_terms = "scRNA|single.cell.RNA|locRNA|lon.non.coding.rna|mirna|microRNA"
+    filterType = "RNA"
+    relevantFileds = ['Title', 'Summary', 'MeSH', 'SampleTerms']
+    
+    def __init__(self, df) -> None:
+        super().__init__(df, self.filterType, self.relevantFileds)
+    
+    ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
+    def filterTerms(self):
+        super().filterTerms(self.regex_terms, self.failedReason, self.successReason)
+    
