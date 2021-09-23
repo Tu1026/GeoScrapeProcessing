3ad84039a20748df52f91f5ed094e459a49fe21e
WilsonTu <s31302@gmail.com>
2021-09-23 15:39:47 -0700

    change to static

--- src/apps/readAndWriter/reader.py
+++ src/apps/readAndWriter/reader.py
@@ -2,21 +2,20 @@
 import pandas as pd
 
 class Reader:
-    def __init__(self) -> None:
-        print("Initiating the reader")
 
-    def pandas_read(self,file, sep):
+    @staticmethod
+    def pandas_read(file, sep="\t"):
         """
         Open up a tab delimited GEOScrape for downstream filtering
         """
         # Pandas to open data frame of our listGEOData output
         df = pd.read_csv(file, sep=sep)
         return(df)
-
-    def open_terms(self,file):
+    @staticmethod
+    def open_terms(file):
         """
         Open up a \n delimited list of terms which you consider hits
         """
         with open(file, "r") as f:
-            my_terms = f.readlines()
+            my_terms = f.read().splitlines()
             return(my_terms)
