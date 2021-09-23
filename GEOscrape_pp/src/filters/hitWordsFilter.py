9055186e9132f034558ae24caac5d5cf3983fb3a
WilsonTu <s31302@gmail.com>
2021-09-23 12:01:54 -0700

    change name

--- GEOscrape_pp/src/filters/hitWordsFilter.py
+++ GEOscrape_pp/src/filters/hitWordsFilter.py
@@ -1,18 +1,20 @@
 from filters import InternalFilter
+from GEOscrape_pp import Config
 
-
-class hitWordFilter(InternalFilter):
+class HitWordFilter(InternalFilter):
     ## The reason why something is filtered out at this stage
     failedReason = "Given Experiment does not match the key word in the files"
     ## Why experiemnt passed filter
     successReason = "Experiment matches the key words given"
     ## The regex terms for unwanted hit terms
-    regex_terms = "scRNA|single.cell.RNA|locRNA|lon.non.coding.rna|mirna|microRNA"
+    regex_terms = ""
     filterType = "RNA"
     relevantFileds = ['Title', 'Summary', 'MeSH', 'SampleTerms']
     
-    def __init__(self, df) -> None:
+    def __init__(self, df, path=Config.getHitTermsFile) -> None:
         super().__init__(df, self.filterType, self.relevantFileds)
+        with open(path, "r") as f:
+            print("success")
     
     ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
     def filterTerms(self):
