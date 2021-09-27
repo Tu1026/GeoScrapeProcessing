ef70e8079c08e1c1985597978bb204f0592df079
WilsonTu <s31302@gmail.com>
2021-09-27 11:42:35 -0700

    Finish implementing the cli

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,11 @@
+from config import Config
 import click
-import pandas as pd
-from apps import Reader
-from apps.filters import RNATypeFilter
-from apps.filters import HitWordFilter
+from apps.geoScrapeMainSwitch import GeoScrapeMainSwitch
 ##Builiding command line options. Cannot figure out how to modularize click options
 
-version = 0.01
+version = 0.5
 softwareName = "Post GEO Filter"
-
+###Build Options
 def add_options(options):
     def _add_options(func):
         for option in reversed(options):
@@ -18,6 +16,11 @@ def add_options(options):
 credentials_option = [click.option('-p','--password', help="GEMMA password"), 
 click.option('-u','--username', required=True, help="GEMMA username")]
 
+geoScrape_option = [click.option('-f', '--file', required=True, help="File that contains the GEOScrape"),
+click.option('-o', '--outPutDir', default=Config.getOutPutDir(),help="Directory where you want to output the processed GEOscrape. Default at /docs/output_files"),
+click.option('-s', '--sep', default="\t", help="Delimiter you want to use for the input and output file"),
+click.option('-h', '--hitWordsFile', default= Config.getHitTermsFile(), help = "Location of the hitTerms file. Default at docs/input_files/terms.txt")]
+
 @click.group()
 def cli():
    pass
@@ -27,11 +30,16 @@ def cli():
 def process(**kwargs):
     print("placeholder")
 
-@cli.command("test")
-def test(**kawargs):
-    new_filt =  HitWordFilter(pd.DataFrame)
-
-
+@cli.command("geoScrape")
+@add_options(geoScrape_option)
+def geoScrape(**kawargs):
+    print("Running with the options and value")
+    for key, value in kawargs.items():
+        if key == "sep":
+            value = f"{value}"
+        print(f"--{key}:{value} ")
+    geoScrapeSwitch = GeoScrapeMainSwitch(kawargs["file"], kawargs["outputdir"], kawargs["hitwordsfile"], kawargs["sep"])
+    geoScrapeSwitch.filterAndOutputFile()
 
 if __name__ == "__main__":
     print('\n')
