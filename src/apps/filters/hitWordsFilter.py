4c2c0f6806b4395c0e44b882e2f5cb2e34b2d6c2
WilsonTu <s31302@gmail.com>
2021-09-27 12:43:31 -0700

    fixed typo

--- src/apps/filters/hitWordsFilter.py
+++ src/apps/filters/hitWordsFilter.py
@@ -10,11 +10,11 @@ class HitWordsFilter(InternalFilter):
     ## The regex terms for unwanted hit terms
     regexTerms = Reader.read_terms(Config.getHitTermsFile())
     filterType = "hitWords"
-    relevantFileds = ['Title', 'Summary', 'MeSH', 'SampleTerms']
+    relevantFields = ['Title', 'Summary', 'MeSH', 'SampleTerms']
     
     def __init__(self, path=Config.getHitTermsFile()) -> None:
-        super().__init__(self.filterType, self.relevantFileds)
+        super().__init__(self.filterType, self.relevantFields)
     
     ### Filter by only using the outputs in Paul's listGEO -> Try out how many false negatives and we can try entrez api?
     def filterTerms(self, df):
-        super().filterTerms(self.regexTerms, self.failedReason, self.successReason)
+        super().filterTerms(df, self.regexTerms, self.failedReason, self.successReason)
