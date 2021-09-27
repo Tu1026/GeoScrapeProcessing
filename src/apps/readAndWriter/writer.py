0615448adb16d3a69459e5b0ee07c14eba6f5a94
WilsonTu <s31302@gmail.com>
2021-09-27 12:44:15 -0700

    added a writer class

---
+++ src/apps/readAndWriter/writer.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from datetime import datetime
+import os
+class Writer:
+    
+
+    @staticmethod
+    def writeToCsv(resultsFrame, sep, outPutDir):
+        print(f"Outputing the file in your selected location at {outPutDir}")
+        currTime = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+        resultsFrame.to_csv(os.path.join(outPutDir,f"Processed_GeoSrape_{currTime}", sep = sep), index=False)
