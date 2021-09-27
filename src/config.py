c93b7456018b51c3ec6cf5877a8343a5cd4fe0e3
WilsonTu <s31302@gmail.com>
2021-09-27 11:01:11 -0700

    implementing the config file

--- src/config.py
+++ src/config.py
@@ -2,14 +2,21 @@ from pathlib import Path
 import os
 
 class Config:
+
     @staticmethod
     def getProjectRootDir():
-        return Path(__file__).parent
+        return Path(__file__).parent.parent
 
     @staticmethod
     def getHitTermsFile():
-        return os.path.join(Config.getProjectRootDir, "docs/input_files/terms.txt")
+        return os.path.join(Config.getProjectRootDir(), "docs/input_files/terms.txt")
 
     @staticmethod
     def getOutPutDir():
-        return os.path.join(Config.getProjectRootDir, "docs/output_files")
+        return os.path.join(Config.getProjectRootDir(), "docs/output_files")
+
+
+    ####For testing only
+    @staticmethod
+    def getTestGEOScrapeFile():
+        return os.path.join(Config.getProjectRootDir(), "docs/input_files/GEOScrape.tsv") 
