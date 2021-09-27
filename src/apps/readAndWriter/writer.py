3a797bdfaf43c5458deb3dcec73ad683006ba63b
WilsonTu <s31302@gmail.com>
2021-09-27 15:13:45 -0700

    fixed bug

--- src/apps/readAndWriter/writer.py
+++ src/apps/readAndWriter/writer.py
@@ -8,4 +8,4 @@ class Writer:
     def writeToCsv(resultsFrame, sep, outPutDir):
         print(f"Outputing the file in your selected location at {outPutDir}")
         currTime = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-        resultsFrame.to_csv(os.path.join(outPutDir,f"Processed_GeoSrape_{currTime}", sep = sep), index=False)
+        resultsFrame.to_csv(os.path.join(outPutDir,f"Processed_GeoSrape_{currTime}"), sep = sep, index=False)
