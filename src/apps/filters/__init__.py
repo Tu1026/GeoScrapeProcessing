600ecf10bd79a870476b27d7a03c7124eec8184a
WilsonTu <s31302@gmail.com>
2021-09-29 14:36:04 -0700

    Removing the filter

--- src/apps/filters/__init__.py
+++ src/apps/filters/__init__.py
@@ -2,7 +2,6 @@ from .internalFilterAbs import InternalFilter
 from .rnaTypeFilter import RNATypeFilter
 from .externalFilterAbs import ExternalFilter
 from .hitWordsFilter import HitWordsFilter
-from .seqTypeFilter import SeqTypeFilter
 from .superSeriesFilter import SuperSeriesFilter
 from .rnaTypeFilter import RNATypeFilter
 from .nonCuratePlatFilter import NonCuratedPlatFilter
