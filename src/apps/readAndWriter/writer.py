e3b1ae23b8a70b2e77f0aaf1311f24a2a57063b6
WilsonTu <s31302@gmail.com>
2021-09-29 14:25:20 -0700

    finish implementing output sheets still have one more output mode needed to implement

--- src/apps/readAndWriter/writer.py
+++ src/apps/readAndWriter/writer.py
@@ -14,9 +14,11 @@ class Writer:
         ## Write main frame
         os.mkdir(os.path.join(outPutDir,currTime))
         resultsFrame.to_csv(os.path.join(outPutDir,f"{currTime}/Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
-        OutputSheetsFormatting.filterOnePlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(1.Ready for loading) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
-        OutputSheetsFormatting.filterMultiPlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(2. Check if you need to split platforms) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
-        OutputSheetsFormatting.nonCuratedPlatFormFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(3. Check if all platforms can be curated) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterOnePlarformCuratableFrameArray(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(1.Ready for loading Arrays) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterOnePlarformCuratableFrameRNASeq(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(2.Ready for loading RNA-seq) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterMultiArrayPlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(3. Check if you need to split platforms Arrays) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterMultiRNASeqPlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(4. Check if you need to split platforms RNA-seq) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.nonCuratedPlatFormFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(5. Check if all platforms can be curated) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
         OutputSheetsFormatting.unwantedFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(Disgarded Experiments) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
 
 
@@ -24,22 +26,45 @@ class Writer:
 
 class OutputSheetsFormatting:
     @staticmethod
-    def filterOnePlarformCuratableFrame(origDf, newDf):
-        print("Preparing single platform curatable list")
+    def filterOnePlarformCuratableFrameArray(origDf, newDf):
+        print("Preparing single array platform curatable list")
         columns = newDf.columns.difference(origDf.columns)
         for column in columns:
             newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
-        newDf.loc[~newDf['Platforms'].str.contains(";")]
+        newDf = newDf.loc[~newDf['Platforms'].str.contains(";")]
+        newDf = newDf.loc[newDf['Type'].str.contains("array")]
         return newDf
 
     @staticmethod
-    def filterMultiPlarformCuratableFrame(origDf, newDf):
+    def filterOnePlarformCuratableFrameRNASeq(origDf, newDf):
+        print("Preparing single RNA seq platform curatable list")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
+        newDf = newDf.loc[~newDf['Platforms'].str.contains(";")]
+        newDf = newDf.loc[~newDf['Type'].str.contains("array")]
+        return newDf
+    
+    @staticmethod
+    def filterMultiArrayPlarformCuratableFrame(origDf, newDf):
         print("Preparing multiplatform curatable list")
         columns = newDf.columns.difference(origDf.columns)
         for column in columns:
             newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
-        newDf.loc[newDf['Platforms'].str.contains(";")]
+        newDf= newDf.loc[newDf['Platforms'].str.contains(";")]
+        newDf = newDf.loc[newDf['Type'].str.contains("array")]
+        return newDf
+        
+    @staticmethod
+    def filterMultiRNASeqPlarformCuratableFrame(origDf, newDf):
+        print("Preparing multiplatform curatable list")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
+        newDf = newDf.loc[newDf['Platforms'].str.contains(";")]
+        newDf = newDf.loc[~newDf['Type'].str.contains("array")]
         return newDf 
+
 
     @staticmethod
     def groupByHitWordsFrame(origDf, newDf):
@@ -51,7 +76,7 @@ class OutputSheetsFormatting:
         print("Preparing non-curated platform experiments list")
         columns = newDf.columns.difference(origDf.columns)
         for column in columns:
-            if column != "nonCuratedPlatforms":
+            if column != "nonCuratedPlatofrms Filter Results":
                 newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
             else:
                 newDf = newDf.loc[newDf[column].str.startswith("(Failure)")]
@@ -61,8 +86,12 @@ class OutputSheetsFormatting:
     def unwantedFrame(origDf, newDf):
         print("Preparing a list of experiments that we cannot or do not want")
         columns = newDf.columns.difference(origDf.columns)
-        for column in columns:
-            if column != "nonCuratedPlatforms":
-                newDf = newDf.loc[newDf[column].str.startswith("(Failure)")]
+        def searchFails(row):
+            for column in columns:
+                if column != "nonCuratedPlatofrms Filter Results":
+                    if row[column].startswith("(Failure"):
+                        return True
+            return False
+        newDf = newDf.loc[newDf.apply(searchFails, axis=1)]
         return newDf
 
