9f110d6744676b5f8b81c85b45785b79711c0575
WilsonTu <s31302@gmail.com>
2021-09-29 14:36:31 -0700

    Adding configuration functions

--- src/config.py
+++ src/config.py
@@ -16,7 +16,19 @@ class Config:
         return os.path.join(Config.getProjectRootDir(), "docs/output_files")
 
 
-    ####For testing only
+    # ####For testing only
+    # @staticmethod
+    # def getTestGEOScrapeFile():
+    #     return os.path.join(Config.getProjectRootDir(), "docs/input_files/GEOScrape.tsv") 
+
     @staticmethod
-    def getTestGEOScrapeFile():
-        return os.path.join(Config.getProjectRootDir(), "docs/input_files/GEOScrape.tsv") 
+    def setThreads():
+        os.environ['OPENBLAS_NUM_THREADS'] = "1"
+
+    @staticmethod
+    def getVersion():
+        return "1.0"
+
+    @staticmethod
+    def getSoftwareName():
+        return "Post GEOScrape Processing"
