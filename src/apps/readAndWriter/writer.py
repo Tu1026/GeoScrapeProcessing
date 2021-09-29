16d92fa0459ce78a9aef8ba3024156d4b56f9167
WilsonTu <s31302@gmail.com>
2021-09-29 11:46:22 -0700

    outline of the writer

--- src/apps/readAndWriter/writer.py
+++ src/apps/readAndWriter/writer.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+import re
 class Writer:
 
     @staticmethod
@@ -11,33 +12,57 @@ class Writer:
         else:
             format="csv"
         ## Write main frame
-        resultsFrame.to_csv(os.path.join(outPutDir,f"/{currTime}Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        os.mkdir(os.path.join(outPutDir,currTime))
+        resultsFrame.to_csv(os.path.join(outPutDir,f"{currTime}/Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterOnePlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(1.Ready for loading) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.filterMultiPlarformCuratableFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(2. Check if you need to split platforms) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.nonCuratedPlatFormFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(3. Check if all platforms can be curated) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
+        OutputSheetsFormatting.unwantedFrame(origFrame, resultsFrame).to_csv(os.path.join(outPutDir,f"{currTime}/(Disgarded Experiments) Processed_GeoSrape_mainFrame.{format}"), sep = sep, index=False)
 
-        ## Get all the ones ready for curation
-        diffColumns = resultsFrame.columns.difference(origFrame.columns)
-        for column in diffColumns:
-            if 
+
 
 
 class OutputSheetsFormatting:
     @staticmethod
-    def filterMainCuratableFrame(origDf, newDf):
-        pass
+    def filterOnePlarformCuratableFrame(origDf, newDf):
+        print("Preparing single platform curatable list")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
+        newDf.loc[~newDf['Platforms'].str.contains(";")]
+        return newDf
 
     @staticmethod
-    def hitWordsGroupingFrame(origDf, newDf):
-        pass
+    def filterMultiPlarformCuratableFrame(origDf, newDf):
+        print("Preparing multiplatform curatable list")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
+        newDf.loc[newDf['Platforms'].str.contains(";")]
+        return newDf 
 
     @staticmethod
     def groupByHitWordsFrame(origDf, newDf):
         pass
+        
 
     @staticmethod
     def nonCuratedPlatFormFrame(origDf, newDf):
-        pass
+        print("Preparing non-curated platform experiments list")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            if column != "nonCuratedPlatforms":
+                newDf = newDf.loc[newDf[column].str.startswith("(Success)")]
+            else:
+                newDf = newDf.loc[newDf[column].str.startswith("(Failure)")]
+        return newDf
 
     @staticmethod
     def unwantedFrame(origDf, newDf):
-        pass
-    
-     
+        print("Preparing a list of experiments that we cannot or do not want")
+        columns = newDf.columns.difference(origDf.columns)
+        for column in columns:
+            if column != "nonCuratedPlatforms":
+                newDf = newDf.loc[newDf[column].str.startswith("(Failure)")]
+        return newDf
+
