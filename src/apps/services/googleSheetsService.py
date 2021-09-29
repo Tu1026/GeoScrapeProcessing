ebf1d521b758fdd0be2d90a40af2447c7591f72e
WilsonTu <s31302@gmail.com>
2021-09-29 16:43:26 -0700

    using index to get sheet

--- src/apps/services/googleSheetsService.py
+++ src/apps/services/googleSheetsService.py
@@ -13,14 +13,14 @@ class GoogleSheetsService:
     def getSpreadSheet(self, sheetUrl):
         self.currSpreadSheet = self.gc.open_by_url(sheetUrl)
 
-    def getWorkSheet(self, sheetName):
-        self.currWorkSheet = self.gc.worksheet(sheetName)
+    def getWorkSheet(self, index):
+        self.currWorkSheet = self.currSpreadSheet.get_worksheet(index)
 
     def createNewWorkSheetFromDf(self, name, df):
         newWorkSheet = self.currSpreadSheet.add_worksheet(title=name, rows=10, cols= 10)
         set_with_dataframe(newWorkSheet, df)
     
-    def getWorkSheetAsFrame(self, sheetName):
-        self.getWorkSheet(sheetName)
+    def getWorkSheetAsFrame(self, index):
+        self.getWorkSheet(index)
         return get_as_dataframe(self.currWorkSheet)
     
