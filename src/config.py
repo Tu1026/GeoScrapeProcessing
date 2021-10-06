fb531696982386ce86080ede611dc7bab9765c2c
WilsonTu <s31302@gmail.com>
2021-10-06 10:30:21 -0700

    Move program initialization to config and update version

--- src/config.py
+++ src/config.py
@@ -2,20 +2,36 @@ from pathlib import Path
 import os
 
 class Config:
-
+    ##Main initializing method should take from click input to set variables
     @staticmethod
-    def iniHitTermsFile(customPath):
+    def initializeProgram(kawargs):
+        Config._iniHitTermsFile(kawargs["hitwordsfile"])
+        Config._iniOutPutDir(kawargs["outputdir"])
+        Config._setThreads()
+        ##Check no overlapping flags
+        if not kawargs['file'] and not kawargs['google']:
+            exit("You need to at least use one of the -f or -g flag")
+
+
+        ##-------------------------------------------------------------------------Below methods should be private and not callable
+    @staticmethod
+    def _iniHitTermsFile(customPath):
         if customPath:
             ConfigVariables.HITTERMSFILE = customPath
         else: 
             ConfigVariables.HITTERMSFILE = os.path.join(ConfigVariables.PROJECTROOTDIR, "docs/input_files/terms.txt")
 
     @staticmethod
-    def iniOutPutDir(customPath):
+    def _iniOutPutDir(customPath):
         if customPath:
            ConfigVariables.OUTPUTDIR = customPath 
         else:
             ConfigVariables.OUTPUTDIR = os.path.join(ConfigVariables.PROJECTROOTDIR, "docs/output_files")
+
+    @staticmethod
+    def _setThreads():
+        os.environ['OPENBLAS_NUM_THREADS'] = "1"
+
 
 
     # ####For testing only
@@ -24,12 +40,8 @@ class Config:
     #     return os.path.join(Config.getProjectRootDir(), "docs/input_files/GEOScrape.tsv") 
 
     @staticmethod
-    def setThreads():
-        os.environ['OPENBLAS_NUM_THREADS'] = "1"
-
-    @staticmethod
     def getVersion():
-        return "1.0"
+        return "1.1"
 
     @staticmethod
     def getSoftwareName():
@@ -40,3 +52,4 @@ class ConfigVariables():
     PROJECTROOTDIR = Path(__file__).parent.parent
     HITTERMSFILE = None
     OUTPUTDIR = None
+
