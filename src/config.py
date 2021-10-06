2cd26dd2df22e7a053913dfa3183ae272a8e0f9d
WilsonTu <s31302@gmail.com>
2021-10-06 10:53:44 -0700

    Initialize every variables in Config class

--- src/config.py
+++ src/config.py
@@ -5,24 +5,25 @@ class Config:
     ##Main initializing method should take from click input to set variables
     @staticmethod
     def initializeProgram(kawargs):
-        Config._iniHitTermsFile(kawargs["hitwordsfile"])
-        Config._iniOutPutDir(kawargs["outputdir"])
+        Config._setHitTermsFile(kawargs["hitwordsfile"])
+        Config._setOutPutDir(kawargs["outputdir"])
         Config._setThreads()
-        ##Check no overlapping flags
-        if not kawargs['file'] and not kawargs['google']:
-            exit("You need to at least use one of the -f or -g flag")
-
-
+        Config._setLoadFileLocation(kawargs['file'])
+        Config._setSeperator(kawargs['sep'])
+        Config._setGoogleUrl(kawargs['google'])
+        Config._setUseHitTermOrNot(kawargs['noterm'])
+        Config._checkNoOverLappingFlags(kawargs['file'], kawargs['google'])
+        Config._printAllVariables(kawargs)
         ##-------------------------------------------------------------------------Below methods should be private and not callable
     @staticmethod
-    def _iniHitTermsFile(customPath):
+    def _setHitTermsFile(customPath):
         if customPath:
             ConfigVariables.HITTERMSFILE = customPath
         else: 
             ConfigVariables.HITTERMSFILE = os.path.join(ConfigVariables.PROJECTROOTDIR, "docs/input_files/terms.txt")
 
     @staticmethod
-    def _iniOutPutDir(customPath):
+    def _setOutPutDir(customPath):
         if customPath:
            ConfigVariables.OUTPUTDIR = customPath 
         else:
@@ -31,7 +32,37 @@ class Config:
     @staticmethod
     def _setThreads():
         os.environ['OPENBLAS_NUM_THREADS'] = "1"
+    
+    @staticmethod
+    def _setLoadFileLocation(fileLocation):
+        ConfigVariables.FILELOCATION = fileLocation
+    
+    @staticmethod
+    def _setUseHitTermOrNot(noHitTerm):
+        ConfigVariables.NOHITTERM = noHitTerm
+    
+    @staticmethod
+    def _setGoogleUrl(url):
+        ConfigVariables.GOOGLEURL = url
 
+    @staticmethod
+    def _setSeperator(sep):
+        ConfigVariables.SEP = sep
+
+    @staticmethod
+    def _checkNoOverLappingFlags(fileLocation, googleUrl):
+        if bool(fileLocation) ^ bool(googleUrl):
+            exit("You need to at least use one of the -f or -g flag but not both")
+
+
+    @staticmethod
+    def _printAllVariables(kawargs):
+        ##Check no overlapping flags
+        print("Running with the options and value")
+        for key, value in kawargs.items():
+            if key == "sep":
+                value = f"{value}"
+            print(f"--{key}:{value} ")
 
 
     # ####For testing only
@@ -52,4 +83,8 @@ class ConfigVariables():
     PROJECTROOTDIR = Path(__file__).parent.parent
     HITTERMSFILE = None
     OUTPUTDIR = None
+    FILELOCATION = None
+    SEP=None
+    GOOGLEURL =None
+    NOHITTERM=False
 
