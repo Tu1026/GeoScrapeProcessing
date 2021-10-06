6c02bec54bd7602224a464c7a7b837a6518bf463
WilsonTu <s31302@gmail.com>
2021-10-06 15:17:59 -0700

    Move the discard part to the initialization and make methods private

--- src/apps/geoScrapeMainSwitch.py
+++ src/apps/geoScrapeMainSwitch.py
@@ -3,29 +3,33 @@ from apps.readAndWriter import Reader, Writer
 from config import ConfigVariables
 
 class GeoScrapeMainSwitch:
-    listOfFilters = {HitWordsFilter, NonCuratedPlatFilter, RNATypeFilter, SuperSeriesFilter, SampleSizeFilter}.discard(ConfigVariables.HITTERMSFILE)
+    listOfFilters = {HitWordsFilter, NonCuratedPlatFilter, RNATypeFilter, SuperSeriesFilter, SampleSizeFilter}
 
     def __init__(self) -> None:
+        ##initiaitng the frame that we will be working with
         if ConfigVariables.GOOGLESERVICE:
             self.geoScrapeFrame = ConfigVariables.GOOGLESERVICE.getWorkSheetAsFrame(0)
         else:
             self.geoScrapeFrame = Reader.pandas_read(ConfigVariables.FILELOCATION, self.sep)
         self.resultsFrame = self.geoScrapeFrame
-        
 
+        ##Remove hitwords filter if user passed in -n
+        self.listOfFilters.discard(ConfigVariables.NOHITTERM)
+
+    ### Main interactable method by outside
     def filterAndOutputFile(self):
         self._runFilters()
         Writer.writeGEOScrapeToCsvs(self.resultsFrame, self.geoScrapeFrame)
         
 
-    def _initalizeFilters(self):
+    def __initalizeFilters(self):
         print("Creating Filters... Please make sure these are the filters you want to use")    
         [print(str(filter)) for filter in self.listOfFilters]
         initializedFilters = [filter() for filter in self.listOfFilters]
         print("Filters initialized")
         return initializedFilters
 
-    def _runFilters(self):
+    def __runFilters(self):
         filters = self._initalizeFilters()
         print("Start running the file on all filters............................")
         for filter in filters:
